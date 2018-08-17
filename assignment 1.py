640183f986139f9c294d351b398007a0240e80c3
Soji-John <42342105+Soji-John@users.noreply.github.com>
2018-08-17 02:36:16 -0700

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,14 @@
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+enter the value of a/n: 11
+enter the value of b/n: 12
+enter the value of c/n: 10
+2354.0
+>>> ================================ RESTART ================================
+>>> 
+what is your name: segun
+how old are you: 12
+segun will be 100 years old in the year 2106
+>>> 
---
+++ eniola.py
@@ -0,0 +1,6 @@
+a = float(input("enter the value of a/n: "))
+b = float(input("enter the value of b/n: "))
+c = float(input("enter the value of c/n: "))
+x = ((-b + 4 * a * c) / 2 * a)
+print(x)
+
